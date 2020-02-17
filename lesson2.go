af6ffe79546bcba71c69fbe765b3721a1442045c
tyakinA <56504404+tyakinA@users.noreply.github.com>
2020-02-17 17:49:20 +0300

    Add files via upload

---
+++ lesson2.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+
+	func main(){
+		var a int
+		fmt.Print("a=")
+		fmt.Scanln(&a)
+
+		if a%2 == 0 {
+					fmt.Println("Четное число")
+		} else {
+					fmt.Println("Нечётное число")
+		}
+	}
